11506c7bcc40bf74cf28d4c5f9de1f30d07707b3
uumair327 <mohdumair.a@somaiya.edu>
2024-07-05 17:12:39 +0530

    v64

--- c++/v64.cpp
+++ c++/v64.cpp
@@ -6,3 +6,16 @@ Template or Generics
 #include <string.h>
 
 using namespace std;
+
+template <class t>
+t sum(t a, t b)
+{
+    return a + b;
+}
+int main()
+{
+    cout << "Int Sum: " << sum(1, 2) << endl;
+    cout << "Float Sum: " << sum(1.1, 2.2) << endl;
+    // cout << "String Sum: " << sum("Hello ", "World") << endl;
+    return 0;
+}
