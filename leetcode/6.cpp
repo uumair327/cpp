359e75a377ecd51764bfc37d05f314d9fd404086
uumair327 <mohdumair.a@somaiya.edu>
2024-07-24 23:10:01 +0530

    6

---
+++ leetcode/6.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#define NOMINMAX
+#include <windows.h>
+#undef NOMINMAX
+#include <string_view>
+#include <string>
+using std::string;
+using std::vector;
+
+class Solution {
+ public:
+  string convert(string s, int numRows) {
+    string ans;
+    vector<vector<char>> rows(numRows);
+    int k = 0;
+    int direction = (numRows == 1) - 1;
+
+    for (const char c : s) {
+      rows[k].push_back(c);
+      if (k == 0 || k == numRows - 1)
+        direction *= -1;
+      k += direction;
+    }
+
+    for (const vector<char>& row : rows)
+      for (const char c : row)
+        ans += c;
+
+    return ans;
+  }
+};
